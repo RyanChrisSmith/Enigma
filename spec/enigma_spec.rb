823952455816abca17cce3cd49fd9ae974a79324
Ryan Smith <ryan.chris.smith@gmail.com>
2022-08-02 16:47:46 -0600

    shell files for beginning of project

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,14 @@
+require 'simplecov'
+require './lib/enigma'
+
+RSpec.describe Enigma do
+
+  before :each do
+    @enigma = Enigma.new
+  end
+
+  it 'exists' do
+    expect(@enigma).to be_a(Enigma)
+  end
+
+end
