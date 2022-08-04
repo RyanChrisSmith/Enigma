7ad87549faca01cc232a74e0ddf8d2036b379ac4
Ryan Smith <ryan.chris.smith@gmail.com>
2022-08-04 17:35:31 -0600

    started encrypt method, it is failing in the right ways. Had some argument errors that made it obvious that most of my tests were written wrong. ALL fixed.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,10 +4,17 @@ require 'key_generatable'
 class Enigma
   include TodaysDatable
   include KeyGeneratable
-  attr_reader :char_set
+  attr_reader :char_set,
+              :message,
+              :key,
+              :date
 
   def initialize
     @char_set = ('a'..'z').to_a << ' '
+  end
+
+  def encrypt(message, key = key_generator, date = todays_date)
+require 'pry' ; binding.pry
   end
 
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -29,29 +29,24 @@ RSpec.describe Enigma do
     expect(mocks.todays_date).to eq("040822")
   end
 
-  xit 'can encrypt a message with optional key and date arguments' do
-    @enigma.encrypt("hello world", "02715", "040895")
-    expect(@enigma.encrypt).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+  it 'can encrypt a message with optional key and date arguments' do
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
   end
 
   xit 'can decrypt a message with key and date' do
-    @enigma.decrypt("keder ohulw", "02715", "040895")
-    expect(@enigma.decrypt).to eq({decryption: "hello world", key: "02715", date: "040895"})
+    expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq({decryption: "hello world", key: "02715", date: "040895"})
   end
 
   xit 'encrypt a message with a key (uses todays date)' do
-    @enigma.encrypt("hello world", "02715")
-    expect(@enigma.encrypt).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+    expect(@enigma.encrypt("hello world", "02715")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
   end
 
   xit 'decrypt a message with a key (uses todays date)' do
-    @enigma.decrypt(encrypted[:encryption], "02715")
-    expect(@enigma.decrypt).to eq({decryption: "hello world", key: "02715", date: "040895"})
+    expect(@enigma.decrypt(encrypted[:encryption], "02715")).to eq({decryption: "hello world", key: "02715", date: "040895"})
   end
 
   xit 'encrypt a message (generates random key and uses todays date)' do
-    @enigma.encrypt("hello world")
-    expect(@enigma.encrypt).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+    expect(@enigma.encrypt("hello world")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
   end
 
 end
