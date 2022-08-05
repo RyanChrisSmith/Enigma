bf1f386624935f45c95e6ddb28b929f0ddef95f4
Ryan Smith <ryan.chris.smith@gmail.com>
2022-08-05 11:17:57 -0600

    working encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -14,7 +14,32 @@ class Enigma
   end
 
   def encrypt(message, key = key_generator, date = todays_date)
-require 'pry' ; binding.pry
+    characters = message.chars
+    offset = (date.to_i ** 2).to_s.slice(-4..-1)
+    a_shift = (offset[0].to_i) + (key[0..1].to_i)
+    b_shift = (offset[1].to_i) + (key[1..2].to_i)
+    c_shift = (offset[2].to_i) + (key[2..3].to_i)
+    d_shift = (offset[3].to_i) + (key[3..4].to_i)
+    encrypted_message = []
+    characters.each_with_index do |character, index|
+      character.downcase!
+      if @char_set.include?(character) == false
+        encrypted_message << character
+      elsif
+        index % 4 == 0
+        encrypted_message << @char_set.rotate(a_shift)[@char_set.index(character)]
+      elsif
+        index % 4 == 1
+        encrypted_message << @char_set.rotate(b_shift)[@char_set.index(character)]
+      elsif
+        index % 4 == 2
+        encrypted_message << @char_set.rotate(c_shift)[@char_set.index(character)]
+      elsif
+        index % 4 == 3
+        encrypted_message << @char_set.rotate(d_shift)[@char_set.index(character)]
+      end
+    end
+    {encryption: encrypted_message.join, key: key, date: date}
   end
 
 end
